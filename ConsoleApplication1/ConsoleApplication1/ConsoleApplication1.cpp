30ddc78c5c5deb29639e07a2259b3db0ad98183e
sjtutsb <sjtutsb@126.com>
2020-09-03 12:27:33 +0800

    ver.1.1_sibin
    The second version.

--- ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,10 +3,13 @@
 
 #include "pch.h"
 #include <iostream>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n"; 
+    cout << "Hello World!\n"; 
+	cout << "suibing tong" << endl;
+	return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
